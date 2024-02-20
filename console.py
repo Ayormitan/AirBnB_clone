8faf9ad1edb862f4798c5b06de0cefb1e0568e30
Ayormitan <kolawoleabaz97@gmail.com>
2024-02-20 02:49:09 -0800

    Console

--- console.py
+++ console.py
@@ -17,5 +17,33 @@ class HBNBCommand (cmd.Cmd):
         """Specifies EOF to also quit the program"""
         print("")
         return True
+    def do_create(self, arg):
+        """
+        Will create class instances ad prints ID.
+        """
+        arg1 = parse(arg)
+        if len(arg1) == 0:
+            print("** class name missing **")
+        elif arg1[0] not in HBNBCommand.__classes:
+            print("** class doesn't exist **")
+        else:
+            print(eval(arg1[0])().id)
+            storage.save()
+        def do_show(self, arg):
+            """ Usage: show <class> <id>
+            Prints the string representation of an instance based on the class name and id.
+            """
+            arg1 = parse(arg)
+            objdict = storage.all()
+            if len(arg1) == 0:
+                print("** class name missing **")
+            elif arg1[0] not in HBNBCommand.__classes:
+                print("** class doesn't exist **")
+            elif len(arg1) == 1:
+                print("** instance id missing **")
+            elif "{}.{}".format(arg1[0],arg1[1]) not in objdict:
+                print("** no instance found **")
+            else:
+                print(objdict["{}.{}".format(arg1[0], arg1[1])])
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
