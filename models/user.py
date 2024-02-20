fdafe71ca7eeec72619deafb8d37fb84c16790da
Ayormitan <kolawoleabaz97@gmail.com>
2024-02-20 12:32:57 -0800

    User profile

---
+++ models/user.py
@@ -0,0 +1,16 @@
+#!/usrbin/python3
+""" class User """
+from models.babse_model import BaseModel
+
+Class User(BaseModel):
+    """ Represent a user.
+    Attributes:
+        email (str): The email of user.
+        password (str): Users password.
+        first_name (str): Users first name
+        last_name (str): User last name
+    """
+    email = ""
+    password = ""
+    first_name = ""
+    last_name = ""
