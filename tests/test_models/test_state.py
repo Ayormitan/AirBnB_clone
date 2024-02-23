00dececc1a354121f7c2023511d9b3bca03d739a
Betvic23 <brightndubisi23@gmail.com>
2024-02-23 06:58:24 -0800

    test user

---
+++ tests/test_models/test_state.py
@@ -0,0 +1,15 @@
+#!/user/bin/python3
+"""Defines unittests for models/state.py.
+
+Unittest classes:
+    TestState_instantiation
+    TestState_save
+    TestState_to_dict
+"""
+
+import os
+import models
+import unittest
+from datetime import datetime
+from time import sleep
+from models.state import state
