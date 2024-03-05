40119872ede416bb1ff024a9c2c4e60b7c1ea160
Ayormitan <kolawoleabaz97@gmail.com>
2024-03-05 02:34:50 -0800

    Filestorage

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -2,6 +2,13 @@
 """Define the filestorage class. """
 import json
 from models.base_model import BaseModel
+from models.user import User
+from models.state import State
+from models.city import City
+from models.place import place
+from models.amenity import Amenity
+from models.review import Review
+
 class FileStorage:
     """ Represent an abstracted storage
     Attributes:
